dcfbd7277dc104c46cbe87333b71decc0a9eff12
Miler Tinta Quispe <miler.tinta@tecsup.edu.pe>
2026-05-04 08:58:43 -0500

    Listo para GitHub

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 console.log("Servidor funcionando");
+require('dotenv').config();
+const http = require('http');
 
+function requestController(req, res) {
+    console.log("Bienvenidos al curso");
+}
+
+const server = http.createServer(requestController);
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+    console.log("Aplicacion corriendo en: " + PORT);
+});
