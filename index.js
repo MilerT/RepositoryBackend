7b07607d5ae9c884e1963d1d22d007d27b06aba5
Miler Tinta Quispe <miler.tinta@tecsup.edu.pe>
2026-05-04 09:23:35 -0500

    Fix PORT correctly

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const http = require('http');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
